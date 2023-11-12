c2bcc6c2da813f3402a0765c27f6582c3a87c326
bi1101 <bihungba1101@GMAIL.COM>
2023-11-12 19:32:55 +0700

    refactor to optimize

--- writify-gform-openai.php
+++ writify-gform-openai.php
@@ -786,10 +786,10 @@ function event_stream_openai(WP_REST_Request $request)
                     //skip
                 }
                 $send_data("[DONE]");
+                // Refresh feeds after processing each feed
+                $feeds = writify_get_feeds($form_id);
                 $send_data("[DIVINDEX-" . $feed_index . "]");
             }
-            // Refresh feeds after processing each feed
-            $feeds = writify_get_feeds($form_id);
             $feed_index++;
         }
 
