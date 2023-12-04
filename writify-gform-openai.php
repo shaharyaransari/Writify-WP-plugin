fcf282ada61d4a9facc4bea8d6e9a4a3007d1929
bi1101 <bihungba1101@GMAIL.COM>
2023-12-04 12:40:43 +0700

    ver bump

--- writify-gform-openai.php
+++ writify-gform-openai.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Writify
  * Description:       Score IELTS Essays x GPT
- * Version:           1.1.0-beta
+ * Version:           1.1.0
  * Author:            IELTS Science
  * Copyright:         © 2023-2026 RLT
  */
